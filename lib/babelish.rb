83eb5fa9edb83d2b539010122a0293b636933542
marcelspinu <marcel.spinu@itsmartsystems.eu>
2022-01-19 15:14:03 +0200

    change csv2strings to convert to swiftgen structure

--- lib/babelish.rb
+++ lib/babelish.rb
@@ -14,6 +14,7 @@ require "babelish/csv2strings"
 require "babelish/csv2android"
 require "babelish/csv2php"
 require "babelish/csv2json"
+require "babelish/swiftgetcsv2string"
 
 # To CSV
 require "babelish/base2csv"
---
+++ lib/babelish/swiftgetcsv2string.rb
@@ -0,0 +1,31 @@
+module Babelish
+  class CSV2Strings < Csv2Base
+    attr_accessor :languages
+
+    def language_filepaths(language)
+      require 'pathname'
+      filepaths = []
+      if language.regions.empty?
+        filepaths << Pathname.new(@output_dir) + "#{language.code}.lproj/#{output_basename}.#{extension}"
+      else
+        language.regions.each do |region|
+          filepaths << Pathname.new(@output_dir) + "#{language.code}-#{region}.lproj/#{output_basename}.#{extension}"
+        end
+      end
+      filepaths
+    end
+
+    def get_row_format(row_key, row_value, comment = nil, indentation = 0)
+      entry = comment.to_s.empty? ? "" : "\n/* #{comment} 123 */\n" 
+      entry + "\"#{row_key}\"" + " " * indentation + " = \"#{row_value}\";\n"
+    end
+
+    def extension
+      "strings"
+    end
+
+    def output_basename
+      @output_basename || 'Localizable'
+    end
+  end
+end
